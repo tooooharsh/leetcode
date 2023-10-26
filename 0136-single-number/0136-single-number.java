3edd82c7c511e4e0bf099d42c39eb4f7a941925a
Harshavardhan Aagale <47719775+tooooharsh@users.noreply.github.com>
2023-10-27 00:02:27 +0530

    Time: 1 ms (99.92%), Space: 44.5 MB (50.59%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+    int output = 0;
+    for (int num : nums) {
+        output ^= num;
+    }
+    return output;
+
+
+
+        
+    }
+}
