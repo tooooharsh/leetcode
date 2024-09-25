52001dddd9a3a430e161bfaadbf5604b700de876
Harshavardhan Aagale <47719775+tooooharsh@users.noreply.github.com>
2024-09-26 03:25:14 +0530

    Time: 12 ms (93.74%), Space: 44.1 MB (82.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-       int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
 
+        //why amount+1? 0 sathi 1 extra space at the beg. and set it to 0.
+        int[] dp = new int[amount + 1];
+        dp[0] = 0; 
+
+        //since min coins aahet, set rest to global maxima
         for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
+            dp[i] = Integer.MAX_VALUE; 
+        }
+
+        // ata taking all eligible coins in account
+        for (int coin : coins) {
+
+            //first eligible coin to amount loop
+
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) { 
+                    //either pick dp[i] or dp[i minus juna coin
+                    // to get min ans]
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
-        
-    }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+}
 }
