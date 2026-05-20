d70268cc9fbbac0a425b3f07caeba5ee099a26b8
Harshavardhan Aagale <47719775+tooooharsh@users.noreply.github.com>
2026-05-21 04:16:34 +0530

    Time: 0 ms (100%), Space: 43 MB (5.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,29 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
+        int t = nums.length;
 
-        if (nums == null || nums.length == 0) return 0;
+        if(t<2) return nums[0];
 
-        if (nums.length == 1) return nums[0];
+        int[] dp = new int[t];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
 
+        for (int i=2; i<t; i++){
+            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
         
-        int n = nums.length;
-        int[] loot = new int[n];
-        
-        //memoize kiya for first 2 indices
-        loot[0] = nums[0];
-        loot[1] = Math.max(nums[0], nums[1]);
-        
-        // logic
-        //looted 2 ho sakti either n-1 or n-2(we will add n into it)
-        //case 1= loot till n-2 = total_loot - looted[n-2] + last wala since we have to maximize
-        //case 2= loot till n-1= total_loot - looted[n-1]
-        //max of case 1 and case 2
-        // max(loot[n-2]+loot[n], loot[n-1])
-        for (int i = 2; i < n; i++) {
-            loot[i] = Math.max( loot[i-2]+ nums[i], loot[i-1]);
         }
+
+        return dp[t-1];
         
-        
-        return loot[n-1];
     }
 }
