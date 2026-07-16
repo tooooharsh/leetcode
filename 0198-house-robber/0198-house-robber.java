83bf7af98df92c2e8ba46acb58ee134a6e1cb83a
Harshavardhan Aagale <47719775+tooooharsh@users.noreply.github.com>
2026-07-17 00:33:04 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        int t = nums.length;
+        int N = nums.length;
 
-        if(t<2) return nums[0];
-
-        int[] dp = new int[t];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-
-        for (int i=2; i<t; i++){
-            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
-        
+        // Special handling for empty array case.
+        if (N == 0) {
+            return 0;
         }
 
-        return dp[t-1];
-        
+        int[] maxRobbedAmount = new int[nums.length + 1];
+
+        // Base case initializations.
+        maxRobbedAmount[N] = 0;
+        maxRobbedAmount[N - 1] = nums[N - 1];
+
+        // DP table calculations.
+        for (int i = N - 2; i >= 0; --i) {
+            // Same as the recursive solution.
+            maxRobbedAmount[i] = Math.max(
+                maxRobbedAmount[i + 1],
+                maxRobbedAmount[i + 2] + nums[i]
+            );
+        }
+
+        return maxRobbedAmount[0];
     }
 }
